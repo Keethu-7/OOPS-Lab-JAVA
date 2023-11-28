9e932078881e96225a4ed2e0dbd25689aaeccdb0
Keerthana J <119199296+Keethu-7@users.noreply.github.com>
2023-11-28 17:51:43 +0530

    FileInputStream & FileOutputStream classes (35)

---
+++ FileInputOutput.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class FileHandling {
+    public static void main(String[] args) throws IOException {
+        String inputFile = "input.txt";
+        String outputFile = "output.txt";
+
+        try {
+            FileInputStream input = new FileInputStream(inputFile);
+            FileOutputStream output = new FileOutputStream(outputFile);
+            int c;
+            while ((c=input.read()) != -1) {
+                output.write(c);
+            }
+            input.close();
+            output.close();
+            System.out.println("File content copied successfully!");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+ 
+    }
+}
