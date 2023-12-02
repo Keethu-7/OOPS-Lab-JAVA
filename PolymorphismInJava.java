1f61c6b145e984ca61c6b76b571c115f05732c25
Keerthana J <119199296+Keethu-7@users.noreply.github.com>
2023-12-02 14:06:46 +0530

    Polymorphism & Abstract class (27)

---
+++ PolymorphismInJava.java
@@ -0,0 +1,32 @@
+abstract class Shape {
+    abstract void numberOfSides();
+}
+
+class Rectangle extends Shape {
+    void numberOfSides() {
+        System.out.println("Rectangle has 4 sides");
+    }
+}
+
+class Triangle extends Shape {
+    void numberOfSides() {
+        System.out.println("Triangle has 3 sides");
+    }
+}
+
+class Hexagon extends Shape {
+    void numberOfSides() {
+        System.out.println("Hexagon has 6 sides");
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Rectangle rect = new Rectangle();
+        Triangle tri = new Triangle();
+        Hexagon hex = new Hexagon();
+        rect.numberOfSides();
+        tri.numberOfSides();
+        hex.numberOfSides();
+    }
+}
